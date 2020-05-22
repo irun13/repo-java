9d483ae6010342dd2652238c5ccc84d81c9f60b2
irun13 <volkova.irka@yandex.ru>
2020-05-22 10:44:56 +0300

    заменила Main.java (Дз2) на Дз3 и удалила 1ую Дз

--- Main.java
+++ Main.java
@@ -1,152 +1,81 @@
 package com.company;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) {
+        // 1. Угадайка чисел.
+        int answer = (int) (Math.random() * 10);
+        int attempts = 3;
+        String playAgain = "N";
+        try {
+            do {
+                System.out.println("Попробуй угадай мое число 0 до 9:)");
+                for (int i = 1; i <= attempts; i++) {
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                        int userAnswer = Integer.parseInt(reader.readLine());
+                        if (userAnswer == answer) {
+                            System.out.println("Огонь, ты угадал!");
+                            break;
+                        } else if (userAnswer > answer) {
+                            if (i == 3) {
+                                System.out.println("Ты проиграл(");
+                                break;
+                            }
+                            System.out.println("Слишком много( У тебя осталось " + (3 - i) + " попытки.");
+                        } else {
+                            if (i == 3) {
+                                System.out.println("Ты проиграл(");
+                                break;
+                            }
+                            System.out.println("Слишком мало( У тебя осталось " + (3 - i) + " попытки.");
+                        }
 
-        System.out.println("1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0");
-        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        System.out.println("Первоночальный массив arr1 " + Arrays.toString(arr1));
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] == 0) {
-                arr1[i] = 1;
-            } else {
-                arr1[i] = 0;
-            }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                // Y больше понравился чем 1 и 0
+                System.out.println("Хочешь сыграть еще? Если да, то введи Y, если нет, то любой другой символ");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                try {
+                    playAgain = reader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } while ("Y".equals(playAgain));
+        } catch (NumberFormatException numberFormatException) {
+            numberFormatException.printStackTrace();
         }
-        System.out.println("Новый массив arr1 " + Arrays.toString(arr1));
-
-        System.out.println("2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21");
-        int[] arr2 = new int[8];
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = i * 3;
-        }
-        System.out.println("Массив arr2 " + Arrays.toString(arr2));
-
-        System.out.println("3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2");
-        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        System.out.println("Первоночальный массив arr3 " + Arrays.toString(arr3));
-        for (int i = 0; i < arr3.length; i++) {
-            if (arr3[i] < 6) {
-                arr3[i] = arr3[i] * 2;
-            }
-        }
-        System.out.println("Новый массив arr3 " + Arrays.toString(arr3));
-
-        System.out.println("4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами");
-        int n = 5; // размер массива типо n x n
-        int[][] arr4 = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    arr4[i][j] = 1;
-                    System.out.print(arr4[i][j] + " ");
+        //2. угадайка слов
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        String answerS = words[(int) (Math.random() * words.length)];
+        System.out.println("Угадай слово, которое я загадал:)");
+        String str = "";
+        for (int i = 0; i < answerS.length(); i++) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                String userAnswerS = reader.readLine();
+                System.out.println(userAnswerS);
+                if (userAnswerS.equals(answerS)) {
+                    System.out.println("Вау! Такое вообще возможно?! Ты угадал!");
+                    break;
                 } else {
-                    arr4[i][j] = 0;
-                    System.out.print(arr4[i][j] + " ");
+                    if (answerS.length() == i + 1) {
+                        System.out.println("Ну комон! Сколько можно?! Не хочу больше играть!) Сам играй!" + "\n" + "Правильный ответ " + answerS);
+                        break;
+                    }
+                    str += answerS.charAt(i);
+                    System.out.println("Подсказка " + (i + 1) + " " + str + "###############" + "\n" + "Попробуй еще.");
                 }
-            }
-            System.out.println();
-        }
-
-        System.out.println("5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета)");
-
-        int[] arr5 = {1, 1, 0, 0, 8, 0, 1, 1, 0, -1};
-        System.out.println("Первоночальный массив arr5 " + Arrays.toString(arr1));
-        int max = arr5[0];
-        int min = arr5[0];
-        for (int i = 0; i < arr1.length; i++) {
-            // ищем max
-            if (max < arr5[i]) {
-                max = arr5[i];
-            }
-            //ищем min
-            if (min > arr5[i]) {
-                min = arr5[i];
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        System.out.println("max = " + max + " min = " + min);
 
-        System.out.println("6. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны. Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true, checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами ||, эти символы в массив не входят.");
-        int[] arr6 = {1, 1, 0, 0, 8, 0, 1, 1, 0, -1};
-        System.out.println("Массив arr6 " + Arrays.toString(arr6));
-        System.out.println(leftAndRightSumm(arr6));
-
-        System.out.println(" 7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным), при этом метод должен сместить все элементымассива на n позиций. Для усложнения задачи нельзя пользоваться вспомогательными массивами.");
-        int[] arr7example = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] arr7 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int shift;
-        System.out.println("Массив arr7 " + Arrays.toString(arr7));
-        //проверка
-        for (int i = -10; i < 12; i++) {
-            spawnArrays(arr7, arr7example); // возвращаем массив в первоночальный вид
-            System.out.println("n = " + i + " " + Arrays.toString(arrayShift(i, arr7)));
-        }
     }
-
-    public static boolean leftAndRightSumm(int arr[]) {
-        int allSumm = arr[0];
-        //считаем сумму цифр в массиве
-        for (int i = 1; i < arr.length; i++) {
-            allSumm += arr[i];
-        }
-        // Сравнивем части, вычитаея из общей суммы левую часть до i
-        int leftSumm = arr[0];
-        int tf = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (allSumm == allSumm - leftSumm) {
-                tf = 1;
-            }
-            leftSumm += arr[i];
-        }
-        if (tf == 1) {
-            return (true);
-        } else {
-            return (false);
-        }
-    }
-
-    public static int[] arrayShift(int n, int[] arr) {
-        int m;
-        // двигаем вправо если minus = 0, т.е. n было положительным числом
-        if (n > 0) {
-            for (int i = 0; i < n; i++) {
-                // запоминаем первый и ставим на его место хвостовой
-                m = arr[0];
-                arr[0] = arr[arr.length - 1];
-                // дергаем массив за хвост
-                for (int j = 1; j < arr.length - 1; j++) {
-                    arr[arr.length - j] = arr[arr.length - j - 1];
-                }
-                // ставим сохраненный элемент в ячейку 1
-                arr[1] = m;
-            }
-        }
-        // двигаем влево
-        if (n < 0) {
-            for (int i = 0; i > n; i--) {
-                m = arr[arr.length - 1];
-                arr[arr.length - 1] = arr[0];
-                for (int j = 1; j < arr.length - 1; j++) {
-                    arr[j - 1] = arr[j];
-                }
-                arr[arr.length - 2] = m;
-            }
-        }
-        return arr;
-    }
-
-    public static int[] spawnArrays(int[] arr, int[] arr7example) {
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr7example[i];
-        }
-        return arr;
-    }
-
 }
-
-
